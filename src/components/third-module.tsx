4b63cc51defe0962ce1819826ad19a9364b8910a
João Pedro Barbosa <43485533+jpbarbosa44@users.noreply.github.com>
2024-06-24 19:51:28 -0300

    Update third-module.tsx

--- src/components/third-module.tsx
+++ src/components/third-module.tsx
@@ -9,7 +9,7 @@ export function ThirdModule() {
             <div className="flex items-center justify-center p-8 bg-[#0c3f8c] flex-col w-[40%]">
                 <img src={cell1} className='w-40 ' alt="" />
                 <p className='text-white mt-4'>
-                    O RoataCerta é o aplicativo de GPS feito especialmente para caminhoneiros, oferecendo tudo o que você precisa para suas viagens de longa distância. Com funcionalidades avançadas e informações centralizadas, o RotaCerta torna suas rotas mais seguras, eficientes e tranquilas.
+                    O RotaCerta é o aplicativo de GPS feito especialmente para caminhoneiros, oferecendo tudo o que você precisa para suas viagens de longa distância. Com funcionalidades avançadas e informações centralizadas, o RotaCerta torna suas rotas mais seguras, eficientes e tranquilas.
                 </p>
             </div>
             <div className='flex w-[60%] bg-[#fcad0b] items-center justify-center gap-4'>
